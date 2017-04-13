7e7342092e9bae669f941a34f82c18075706424b
macster00 <dmcalister00@gmail.com>
2017-04-13 14:50:34 -0400

    working on phase 3 ProductTable

--- src/java/edu/uncc/nbad/ProductTable.java
+++ src/java/edu/uncc/nbad/ProductTable.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 import java.util.StringTokenizer;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -15,7 +16,16 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class ProductTable {
 
 	
+    static String url = "jdbc:mysql://localhost:3306/shop";  //I called my db "shop"
+    static String username = "project";
+    static String password = "1234";
+
+    static Connection connection = null;
+    static PreparedStatement selectProducts = null;
+    static ResultSet resultset = null;
+    
     static {
+        System.out.println("this was called");
         try {
             Class.forName("com.mysql.jdbc.Driver");
         }
@@ -23,15 +33,59 @@ public class ProductTable {
             System.out.println(e.getMessage());
             System.exit(1);
         }
+        
+        try {
+        connection = DriverManager.getConnection(url, username, password);
+
+        }
+        catch (SQLException e){
+            for (Throwable t: e)
+                t.printStackTrace();
+        }
 
     }
-
+    
+   
     public static List<Product> selectProducts() {
-		throw new NotImplementedException(); // remove this line and implement the logic
+        System.out.println("get list");
+        Product product = null;
+        
+        
+        String qString = "SELECT * FROM products";
+        try {
+            selectProducts = connection.prepareStatement(qString);
+            //selectProduct.setInt(1, id);
+            resultset = selectProducts.executeQuery();
+            ArrayList<Product> productList = new ArrayList<>();
+            while (resultset.next())
+            {
+                product = new Product();
+                product.setCode(resultset.getString("code"));
+                product.setDescription(resultset.getString("description"));
+                product.setPrice(resultset.getDouble("price"));
+                productList.add(product);
+            
+            }  
+            return productList;
+        }
+        catch(SQLException e){
+            System.out.println(e);
+            return null;
+        }
     }
 
     public static Product selectProduct(String productCode) {
-		throw new NotImplementedException(); // remove this line and implement the logic
+        System.out.println("get single product");
+        Product product = null;
+        try {
+        connection = DriverManager.getConnection(url, username, password);
+
+        }
+        catch (SQLException e){
+            for (Throwable t: e)
+                t.printStackTrace();
+        }
+        return null;
     }
 
     public static boolean exists(String productCode) {
