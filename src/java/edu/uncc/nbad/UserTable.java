ac45c88edb2e85e4665edbe3b706b9b17b0a59c8
Kassandra Walker <Kassandra Walker@172.16.179.129>
2017-04-14 17:34:02 -0400

    UserTable - add Record

--- src/java/edu/uncc/nbad/UserTable.java
+++ src/java/edu/uncc/nbad/UserTable.java
@@ -1,26 +1,56 @@
 package edu.uncc.nbad;
+// implemented by Kassandra Walker
 
 import java.io.*;
 import java.util.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class UserTable {
-	
-	//Static initializer, it runs when the class is intialized (it is executed once)
+
+    private static String url = "jdbc:mysql://localhost:3306/shop";  //I called my db "shop"
+    private static String username = "project";
+    private static String password = "1234";
+
+    private static Connection connection = null;
+    private static PreparedStatement ps = null;
+    private static ResultSet rs = null;
+
+    //Static initializer, it runs when the class is intialized (it is executed once)
     static {
+        // connects to the driver
         try {
             Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UserTable.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch (ClassNotFoundException e) {
+        // connects to the database
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            for (Throwable t : e) {
+                t.printStackTrace();
+            }
             System.out.println(e.getMessage());
             System.exit(1);
         }
 
     }
-    
-    public static void addRecord(User user) throws IOException {
-		throw new NotImplementedException(); // remove this line and implement the logic
 
+    public static void addRecord(User user) throws IOException {
+        String SQL = "INSERT INTO `users` (firstName, lastName, email, password) VALUES (?, ?, ?)";
+        try {
+            ps = connection.prepareStatement(SQL);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getEmail());
+            ps.setString(4, user.getPassword());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Exception thrown: " + e);
+        }
     }
 
     public static User getUser(String emailAddress) throws IOException {
@@ -29,10 +59,10 @@ public class UserTable {
     }
 
     public static ArrayList<User> getUsers() throws IOException {
-		throw new NotImplementedException(); // remove this line and implement the logic
+        throw new NotImplementedException(); // remove this line and implement the logic
     }
 
     public static HashMap<String, User> getUsersMap() throws IOException {
-		throw new NotImplementedException(); // remove this line and implement the logic
+        throw new NotImplementedException(); // remove this line and implement the logic
     }
 }
